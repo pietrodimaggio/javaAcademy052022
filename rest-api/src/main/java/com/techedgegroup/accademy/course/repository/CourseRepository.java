b6202cf59915d0dca58e9e30fd7bc437f1467742
Alessandro Riva <alessandro.riva@techedgegroup.com>
2022-05-04 14:33:05 +0200

    JPA Projection

--- rest-api/src/main/java/com/techedgegroup/accademy/course/repository/CourseRepository.java
+++ rest-api/src/main/java/com/techedgegroup/accademy/course/repository/CourseRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import com.techedgegroup.accademy.course.datamodel.Course;
+import com.techedgegroup.accademy.course.datamodel.CourseSummary;
 
 public interface CourseRepository extends JpaRepository<Course, Integer> {
 
@@ -15,5 +16,8 @@ public interface CourseRepository extends JpaRepository<Course, Integer> {
 
 	@Query(value = "SELECT DISTINCT CATEGORY FROM COURSES", nativeQuery = true)
 	List<String> findAllCategories();
+
+	 @Query(value = "SELECT new com.techedgegroup.accademy.course.datamodel.CourseSummary(c.id, c.courseName, c.courseCategory, c.courseDate, c.teacher.teacherSurname, size(c.students)) FROM Course c")
+	    List<CourseSummary> getCourseSummary();
 
 }
