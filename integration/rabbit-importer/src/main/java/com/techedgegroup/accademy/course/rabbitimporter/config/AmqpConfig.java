6fbf5ddce45568b3221b82047c32fac478033dca
Alessandro Riva <alessandro.riva@techedgegroup.com>
2022-05-05 16:10:25 +0200

    Correzione

--- integration/rabbit-importer/src/main/java/com/techedgegroup/accademy/course/rabbitimporter/config/AmqpConfig.java
+++ integration/rabbit-importer/src/main/java/com/techedgegroup/accademy/course/rabbitimporter/config/AmqpConfig.java
@@ -1,30 +1,15 @@
 package com.techedgegroup.accademy.course.rabbitimporter.config;
 
-import org.springframework.amqp.rabbit.annotation.RabbitListenerConfigurer;
-import org.springframework.amqp.rabbit.listener.RabbitListenerEndpointRegistrar;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.converter.MappingJackson2MessageConverter;
-import org.springframework.messaging.handler.annotation.support.DefaultMessageHandlerMethodFactory;
-import org.springframework.messaging.handler.annotation.support.MessageHandlerMethodFactory;
 
 @Configuration
-public class AmqpConfig implements RabbitListenerConfigurer {
+public class AmqpConfig {
 
-    @Override
-    public void configureRabbitListeners(RabbitListenerEndpointRegistrar registrar) {
-        registrar.setMessageHandlerMethodFactory(messageHandlerMethodFactory());
-    }
 
     @Bean
-    MessageHandlerMethodFactory messageHandlerMethodFactory() {
-        DefaultMessageHandlerMethodFactory messageHandlerMethodFactory = new DefaultMessageHandlerMethodFactory();
-        messageHandlerMethodFactory.setMessageConverter(consumerJackson2MessageConverter());
-        return messageHandlerMethodFactory;
-    }
-
-    @Bean
-    public MappingJackson2MessageConverter consumerJackson2MessageConverter() {
-        return new MappingJackson2MessageConverter();
+    public Jackson2JsonMessageConverter  consumerJackson2MessageConverter() {
+        return new Jackson2JsonMessageConverter ();
     }
 }
